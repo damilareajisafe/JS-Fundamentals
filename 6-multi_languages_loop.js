4ed9de6a4318c2a3c5e6c36715c5b7f4c1fdace9
Damilare Ajisafe <dammyajisafe@gmail.com>
2025-07-14 06:51:22 +0100

    modify 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 let langs = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for(let i = 0; i < langs.length; i++) {
+for (let i = 0; i < langs.length; i++) {
     console.log(langs[i]);
 }
